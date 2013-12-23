aec7ee557ab104b89126af105065ea7932c39741
Serge Hallyn <serge.hallyn@ubuntu.com>
2013-12-23 11:57:42 -0600

    test6: account for our own cgroup
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- tests/test06.sh
+++ tests/test06.sh
@@ -12,7 +12,8 @@ if [ $? -ne 0 ]; then
 fi
 # confirm that it was moved
 c=`cat /proc/$pid/cgroup | grep memory | awk -F: '{ print $3 }'`
-if [ "$c" != "/xxx/b" ]; then
+myc=`cat /proc/$$/cgroup | grep memory | awk -F: '{ print $3 }'`
+if [ "$c" != "${myc}/xxx/b" ]; then
 	kill -9 $pid
 	exit 1
 fi
