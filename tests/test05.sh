4bfc0a7ccd0576acc7e3b99d97319827930d12ef
Serge Hallyn <serge.hallyn@ubuntu.com>
2013-12-23 14:05:35 -0600

    test5: ../xxx/b certainly could exist
    At least use a cgroup name which we don't create in the run
    of the testsuite.
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- tests/test05.sh
+++ tests/test05.sh
@@ -2,7 +2,8 @@
 
 echo "Test 5 (../ getValue)"
 #This should fail:
-dbus-send --print-reply --address=unix:path=/tmp/cgmanager --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.getValue string:'memory' string:"../xxx/b" string:'memory.usage_in_bytes' > /dev/null 2>&1
+# Note, if ../xxx0123/b exists, it'll succeed and the test will report failure
+dbus-send --print-reply --address=unix:path=/tmp/cgmanager --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.getValue string:'memory' string:"../xxx0123/b" string:'memory.usage_in_bytes' > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	exit 1
 fi
