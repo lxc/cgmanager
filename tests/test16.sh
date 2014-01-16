96195d28efde83b2bd03fd0a46002a8ab4f272c5
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-01-16 12:21:36 -0600

    add test of INvalid unprivileged cgroup setting
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

---
+++ tests/test16.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ -n "$SUDO_USER" ]; then
+	gid=$SUDO_GID
+	uid=$SUDO_UID
+else
+	gid=1000
+	uid=1000
+fi
+
+echo "test 16: INvalid unprivileged setvalue"
+
+# Make sure zzz/b is new so it has a full limit to begin with
+dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.Remove string:'memory' string:'zzz' int32:1 || true
+dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.Create string:'memory' string:'zzz/b'
+dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.chownCgroup string:'memory' string:'zzz/b' int32:$uid int32:0
+
+# Now $uid can create under zzz/b, but should NOT be able to change limits in zzz/b itself
+
+new=99999
+sudo -u \#$uid dbus-send --print-reply --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.setValue string:'memory' string:'zzz/b' string:'memory.limit_in_bytes' string:"$new" > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	echo "test 16: should have failed to set limit_in_bytes!"
+	exit 1
+fi
+
+exit 0
