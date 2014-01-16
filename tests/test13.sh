63b50a95f7253d496ee5e1c7d6eba1fe5e3709a3
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-01-16 12:17:21 -0600

    test13: setup correctly
    remove xxx/b;  chown xxx to $uid;  create xxx/b as $uid;  now xxx/b's
    settings can be modified by $uid, and xxx/b starts out with full
    memory limit.  So $uid can change the limit, and we can check whether
    the new limit was set.
    
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- tests/test13.sh
+++ tests/test13.sh
@@ -9,15 +9,12 @@ fi
 
 echo "test 13: valid unprivileged setvalue"
 
-myc=`cat /proc/$$/cgroup | grep memory | awk -F: '{ print $3 }'`
-cantmount=0
-mount -t cgroup -o memory cgroup /sys/fs/cgroup || cantmount=1
-if [ $cantmount -eq 0 ]; then
-	prev=`cat /sys/fs/cgroup/${myc}/zzz/b/memory.limit_in_bytes`
-	umount /sys/fs/cgroup
-else
-	prev=`dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.getValue string:'memory' string:'zzz/b' string:'memory.usage_in_bytes' | awk '{ print $1}'`
-fi
+# Make sure zzz/b is new so it has a full limit to begin with
+dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.Remove string:'memory' string:'zzz/b' int32:1 || true
+dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.chownCgroup string:'memory' string:'zzz' int32:$uid int32:0
+sudo -u \#$uid dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.Create string:'memory' string:'zzz/b'
+
+prev=`dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.getValue string:'memory' string:'zzz/b' string:'memory.limit_in_bytes' | awk '{ print $1}'`
 
 new=99999
 if [ "$prev" = "102400" ]; then
@@ -29,13 +26,7 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-if [ $cantmount -eq 0 ]; then
-	mount -t cgroup -o memory cgroup /sys/fs/cgroup
-	after=`cat /sys/fs/cgroup/${myc}/zzz/b/memory.limit_in_bytes`
-	umount /sys/fs/cgroup
-else
-	after=`dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.getValue string:'memory' string:'zzz/b' string:'memory.usage_in_bytes' | awk '{ print $1}'`
-fi
+after="`dbus-send --print-reply=literal --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.getValue string:'memory' string:'zzz/b' string:'memory.limit_in_bytes' | awk '{ print $1 }'`"
 if [ "$prev" = "$after" ]; then
 	echo "test 13: old limit was $prev, new is $after"
 	exit 1
