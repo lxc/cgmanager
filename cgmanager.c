8236b18890677a1fd17a199389fdecfa26400fac
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-01-16 11:56:37 -0600

    cgmanager: write trailing null when writing a string
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- cgmanager.c
+++ cgmanager.c
@@ -164,7 +164,7 @@ static void get_pid_scm_reader (struct scm_sock_data *data,
 	nih_info (_("Victim is pid=%d"), target_pid);
 
 	if (!get_pid_cgroup_main(data, controller, target_pid, ucred, &output))
-		ret = write(data->fd, output, strlen(output));
+		ret = write(data->fd, output, strlen(output)+1);
 	else
 		ret = write(data->fd, &ucred, 0);  // kick the client
 	if (ret < 0)
@@ -918,7 +918,7 @@ static void get_value_scm_reader (struct scm_sock_data *data,
 		  data->fd, ucred.pid, ucred.uid, ucred.gid);
 
 	if (!get_value_main(data, data->controller, data->cgroup, data->key, ucred, &output))
-		ret = write(data->fd, output, strlen(output));
+		ret = write(data->fd, output, strlen(output)+1);
 	else
 		ret = write(data->fd, &ucred, 0);  // kick the client
 	if (ret < 0)
