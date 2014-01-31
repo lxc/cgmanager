a8110cf87749787136aec402e9f15a41f3df8d59
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-01-31 09:19:29 +0000

    test09: use tmpdir for mounting
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- tests/test09.sh
+++ tests/test09.sh
@@ -12,14 +12,22 @@ else
 	uid=1000
 fi
 
+mnt=`mktemp -d`
+
+cleanup() {
+	umount $mnt || true
+	rmdir $mnt
+}
+
+trap cleanup EXIT
+
 # We can't readily verify if we can't mount cgroups
 cantmount=0
-mount -t cgroup -o memory cgroup /sys/fs/cgroup || cantmount=1
+mount -t cgroup -o memory cgroup $mnt || cantmount=1
 if [ $cantmount -eq 0 ]; then
 	myc=`cat /proc/$$/cgroup | grep memory | awk -F: '{ print $3 }'`
-	rmdir /sys/fs/cgroup/${myc}/zzz/b || true
-	rmdir /sys/fs/cgroup/${myc}/zzz || true
-	umount /sys/fs/cgroup
+	rmdir "${mnt}/${myc}/zzz/b" || true
+	rmdir "${mnt}/${myc}/zzz" || true
 fi
 
 dbus-send --print-reply --address=unix:path=/sys/fs/cgroup/cgmanager/sock --type=method_call /org/linuxcontainers/cgmanager org.linuxcontainers.cgmanager0_0.Create string:'memory' string:"zzz" > /dev/null 2>&1
@@ -28,12 +36,11 @@ if [ $cantmount -eq 1 ]; then
 	echo "Chowned zzz, but cannot verify the result"
 	exit 0
 fi
-mount -t cgroup -o memory cgroup /sys/fs/cgroup
-o1=`stat --format="%u:%g" /sys/fs/cgroup/${myc}/zzz`
-o2=`stat --format="%u:%g" /sys/fs/cgroup/${myc}/zzz/tasks`
-o3=`stat --format="%u:%g" /sys/fs/cgroup/${myc}/zzz/cgroup.procs`
-o4=`stat --format="%u:%g" /sys/fs/cgroup/${myc}/zzz/memory.limit_in_bytes`
-umount /sys/fs/cgroup
+o1=`stat --format="%u:%g" ${mnt}/${myc}/zzz`
+o2=`stat --format="%u:%g" ${mnt}/${myc}/zzz/tasks`
+o3=`stat --format="%u:%g" ${mnt}/${myc}/zzz/cgroup.procs`
+o4=`stat --format="%u:%g" ${mnt}/${myc}/zzz/memory.limit_in_bytes`
+
 if [ "$o1" != "$uid:$gid" ]; then
 	exit 1
 fi
