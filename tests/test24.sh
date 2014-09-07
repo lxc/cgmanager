da18cc64f783526484e9a636332f00bcc3489a8b
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-09-07 11:00:03 -0500

    account for premounting sadness in test24
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

--- tests/test24.sh
+++ tests/test24.sh
@@ -30,15 +30,30 @@ if cgm listchildren freezer prune0 2>/dev/null; then
 fi
 
 # Test that prune effects after all tasks die
+cgm create freezer prune1
+sleep 3 &
+pid=$!
+cgm movepid freezer prune1 $pid
+cgm prune freezer prune1
+wait $!
+sleep 1 # give buggy kernels a chance
+if cgm listchildren freezer prune1 2>/dev/null; then
+	echo "prune failed to effect remove-on-empty"
+	exit 1
+fi
+
+# Same, but recursive
+# This will fail if cgroup has been premounted.  
 cgm create freezer prune1/prune2
-sleep 5 &
+sleep 3 &
 pid=$!
 cgm movepid freezer prune1/prune2 $pid
 cgm prune freezer prune1
 wait $!
 sleep 1 # give buggy kernels a chance
-if cgm listchildren freezer prune1; then
-	echo "prune failed to effect remove-on-empty"
+if cgm listchildren freezer prune1 2>/dev/null; then
+	echo "prune failed to recursively remove-on-empty"
+	echo "if freezer was not premounted, this is a bug"
 	exit 1
 fi
 
