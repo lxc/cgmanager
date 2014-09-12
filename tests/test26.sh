beb684c2a66f03e57ad6b5123d6eb64a2cae1840
Serge Hallyn <serge.hallyn@ubuntu.com>
2014-09-12 14:14:45 -0500

    add simple listkeys testcase
    Signed-off-by: Serge Hallyn <serge.hallyn@ubuntu.com>

---
+++ tests/test26.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Test 25: listkeys"
+
+cgm remove memory listkeys1 || true
+
+sleep 1
+
+cgm create memory listkeys1
+cgm chown memory listkeys1 100000 100000
+
+output=$(cgm listkeys memory listkeys1)
+
+id1=$(echo "$output" | awk '/^tasks/ { print $2 }')
+if [ "$id1" != "100000" ]; then
+	echo "Bad listkeys output"
+	false
+fi
+
+echo PASS
